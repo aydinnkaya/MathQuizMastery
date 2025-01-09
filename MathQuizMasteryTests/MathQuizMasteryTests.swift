ce3cef73cf297f45f284a824d71d444f5c5c93c8
Aydın Kaya <Aydinnkaya1@gmail.com>
2025-01-09 10:13:58 +0300

    Initial Commit

---
+++ MathQuizMasteryTests/MathQuizMasteryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuizMasteryTests.swift
+//  MathQuizMasteryTests
+//
+//  Created by AydınKaya on 9.01.2025.
+//
+
+import Testing
+@testable import MathQuizMastery
+
+struct MathQuizMasteryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
