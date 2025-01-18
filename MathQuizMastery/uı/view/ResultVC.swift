1eaa0f6f4a9bfbef64de4c718d45a4790857c3fe
aydinnkaya <Aydinnkaya1@gmail.com>
2025-01-18 13:42:09 +0300

    updateView

--- MathQuizMastery/uı/view/ResultVC.swift
+++ MathQuizMastery/uı/view/ResultVC.swift
@@ -10,12 +10,17 @@ import UIKit
 class ResultVC: UIViewController {
     @IBOutlet weak var label: UILabel!
     
+    @IBOutlet weak var kategoriButtonLabel: UIButton!
     var receivedScore: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
  
         label.text = receivedScore
+        
+        
+        kategoriButtonLabel.layer.cornerRadius = 20
+        kategoriButtonLabel.clipsToBounds = true
         
     }
 }
