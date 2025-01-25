92436468ab27eb9bf744673d7ee55fe4d596954f
aydinnkaya <Aydinnkaya1@gmail.com>
2025-01-25 23:08:16 +0300

    buttonUI Update

--- MathQuizMastery/uı/view/authentication/LoginVC.swift
+++ MathQuizMastery/uı/view/authentication/LoginVC.swift
@@ -27,7 +27,7 @@ class LoginVC: UIViewController, UITextFieldDelegate {
         
         configureTextFieldBackground(for: emailTextField)
         configureTextFieldBackground(for: passwordTextField)
-        
+        configureButton(loginButtonLabel)
         emailTextField.delegate = self
         passwordTextField.delegate = self
         
@@ -59,6 +59,8 @@ class LoginVC: UIViewController, UITextFieldDelegate {
         
         configureTextFieldBackground(for: emailTextField)
         configureTextFieldBackground(for: passwordTextField)
+        configureButton(loginButtonLabel)
+        
     }
     
 }
@@ -82,6 +84,46 @@ extension LoginVC {
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+    
+    func configureButton(_ button : UIButton){
+        
+        button.layer.cornerRadius = 12
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor(red: 1.0, green: 0.8627, blue: 0.0, alpha: 1.0).cgColor
+        button.backgroundColor = .clear
+        
+        
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.purple.cgColor,
+            UIColor.red.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        
+        
+        
+        if let superview = button.superview {
+            let adjustedFrame = superview.convert(button.frame, to: superview)
+            let backgroundView = UIView(frame: adjustedFrame)
+            gradientLayer.frame = CGRect(origin: .zero, size: adjustedFrame.size)
+            gradientLayer.cornerRadius = button.layer.cornerRadius
+            
+            backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+            backgroundView.layer.cornerRadius = button.layer.cornerRadius
+            backgroundView.layer.shadowColor = UIColor.purple.cgColor
+            backgroundView.layer.shadowOffset = CGSize(width: 0, height: 3)
+            backgroundView.layer.shadowOpacity = 0.3
+            backgroundView.layer.shadowRadius = 6
+            backgroundView.layer.masksToBounds = false
+            
+            superview.addSubview(backgroundView)
+            superview.bringSubviewToFront(button)
+        }
+        
     }
     
     func configureTextField(_ textField: UITextField, placeholderText: String, iconName: String) {
@@ -116,6 +158,7 @@ extension LoginVC {
         textField.superview?.layer.sublayers?
             .filter { $0 is CAGradientLayer }
             .forEach { $0.removeFromSuperlayer() }
+        
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [
             UIColor(red: 0.8, green: 0.85, blue: 0.9, alpha: 1).cgColor,
--- MathQuizMastery/uı/view/authentication/RegisterVC.swift
+++ MathQuizMastery/uı/view/authentication/RegisterVC.swift
@@ -24,7 +24,8 @@ class RegisterVC: UIViewController, UITextFieldDelegate {
             self.configureTextFieldBackground(for: self.emailTextField)
             self.configureTextFieldBackground(for: self.passwordTextField)
             self.configureTextFieldBackground(for: self.passwordAgainTextField)
-            
+            self.configureButton(self.cerateAccountLabel)
+
             self.configureTextField(self.nameTextField, placeholderText: "Enter your name", iconName: "person.fill")
             self.configureTextField(self.emailTextField, placeholderText: "Enter your email", iconName: "envelope.fill")
             self.configureTextField(self.passwordTextField, placeholderText: "Enter your password", iconName: "lock.fill")
@@ -49,6 +50,7 @@ class RegisterVC: UIViewController, UITextFieldDelegate {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        configureButton(cerateAccountLabel)
         configureTextFieldBackground(for: nameTextField)
         configureTextFieldBackground(for: emailTextField)
         configureTextFieldBackground(for: passwordTextField)
@@ -76,6 +78,46 @@ extension RegisterVC {
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
         self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    
+    func configureButton(_ button : UIButton){
+        
+        button.layer.cornerRadius = 12
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor(red: 1.0, green: 0.8627, blue: 0.0, alpha: 1.0).cgColor
+        button.backgroundColor = .clear
+        
+        
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.purple.cgColor,
+            UIColor.red.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        
+        
+        
+        if let superview = button.superview {
+            let adjustedFrame = superview.convert(button.frame, to: superview)
+            let backgroundView = UIView(frame: adjustedFrame)
+            gradientLayer.frame = CGRect(origin: .zero, size: adjustedFrame.size)
+            gradientLayer.cornerRadius = button.layer.cornerRadius
+            
+            backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+            backgroundView.layer.cornerRadius = button.layer.cornerRadius
+            backgroundView.layer.shadowColor = UIColor.purple.cgColor
+            backgroundView.layer.shadowOffset = CGSize(width: 0, height: 3)
+            backgroundView.layer.shadowOpacity = 0.3
+            backgroundView.layer.shadowRadius = 6
+            backgroundView.layer.masksToBounds = false
+            
+            superview.addSubview(backgroundView)
+            superview.bringSubviewToFront(button)
+        }
+        
     }
     
     func configureTextField(_ textField: UITextField, placeholderText: String, iconName: String) {
