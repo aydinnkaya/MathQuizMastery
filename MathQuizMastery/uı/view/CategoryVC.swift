140bb64c82cce0cd2e2cb604093585f2cf84819a
aydinnkaya <Aydinnkaya1@gmail.com>
2025-01-26 23:56:58 +0300

    updateUı

--- MathQuizMastery/uı/view/CategoryVC.swift
+++ MathQuizMastery/uı/view/CategoryVC.swift
@@ -9,21 +9,69 @@ import UIKit
 
 class CategoryVC: UIViewController {
 
+    @IBOutlet weak var ButtonToplamaLabel: UIButton!
+    
+    @IBOutlet weak var buttonÇıkarmaLabel: UIButton!
+    
+    @IBOutlet weak var buttonÇarpmaLabel: UIButton!
+    
+    @IBOutlet weak var buttonBölmeLabel: UIButton!
+    
+    @IBOutlet weak var buttonKarisikLabel: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureButton(ButtonToplamaLabel)
+        configureButton(buttonÇıkarmaLabel)
+        configureButton(buttonÇarpmaLabel)
+        configureButton(buttonBölmeLabel)
+        configureButton(buttonKarisikLabel)
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension CategoryVC {
+    
+    func configureButton(_ button : UIButton){
+        
+        button.layer.cornerRadius = 12
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor(red: 1.0, green: 0.8627, blue: 0.0, alpha: 1.0).cgColor
+        button.backgroundColor = .clear
+        
+        
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.purple.cgColor,
+            UIColor.red.cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        
+        
+        
+        if let superview = button.superview {
+            let adjustedFrame = superview.convert(button.frame, to: superview)
+            let backgroundView = UIView(frame: adjustedFrame)
+            gradientLayer.frame = CGRect(origin: .zero, size: adjustedFrame.size)
+            gradientLayer.cornerRadius = button.layer.cornerRadius
+            
+            backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+            backgroundView.layer.cornerRadius = button.layer.cornerRadius
+            backgroundView.layer.shadowColor = UIColor.purple.cgColor
+            backgroundView.layer.shadowOffset = CGSize(width: 0, height: 3)
+            backgroundView.layer.shadowOpacity = 0.3
+            backgroundView.layer.shadowRadius = 6
+            backgroundView.layer.masksToBounds = false
+            
+            superview.addSubview(backgroundView)
+            superview.bringSubviewToFront(button)
+        }
+        
     }
-    */
-
 }
