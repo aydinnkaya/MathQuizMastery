572fdb802c4c8ea42cccdccbeba7f778040a933b
aydinnkaya <Aydinnkaya1@gmail.com>
2025-03-17 23:17:46 +0300

    UserInfoStackViewCreated

--- MathQuizMastery/uı/view/CategoryVC.swift
+++ MathQuizMastery/uı/view/CategoryVC.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import SwiftUICore
 
 class CategoryVC: UIViewController {
     
+    @IBOutlet weak var label: UILabel!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var buttonToplama: UIButton!
     @IBOutlet weak var buttonCikarma: UIButton!
@@ -19,27 +21,52 @@ class CategoryVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupStackView()
-        setupButtons()
-    }
-    
-    private func setupStackView() {
-        stackView.alignment = .center
-        stackView.distribution = .equalSpacing
-        stackView.spacing = 16
-    }
-    
-    private func setupButtons() {
-        let titles = ["Toplama", "Çıkarma", "Çarpma", "Bölme", "Karışık"]
-        let expressions: [MathExpression.ExpressionType] = [.addition, .subtraction, .multiplication, .division, .mixed]
+        applyGradientBackground()
+               configureButton(buttonToplama, title: "Toplama", imageName: "addition_icon")
+               configureButton(buttonCikarma, title: "Çıkarma", imageName: "subtraction_icon")
+               configureButton(buttonCarpma, title: "Çarpma", imageName: "multiplication_icon")
+               configureButton(buttonBolme, title: "Bölme", imageName: "division_icon")
+               configureButton(buttonKarisik, title: "Karışık", imageName: "mixed_icon")
         
-        for (index, title) in titles.enumerated() {
-            let button = createHexButton(title: title)
-            button.tag = index
-            button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
-            stackView.addArrangedSubview(button)
-        }
     }
+    
+    
+    
+    
+    
+    
+    
+    func applyGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            
+            UIColor(red: 200/255, green: 150/255, blue: 50/255, alpha: 1).cgColor,
+            UIColor(red: 80/255, green: 80/255, blue: 80/255, alpha: 1).cgColor,
+            UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1).cgColor,
+            UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 1).cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = view.bounds
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    /*
+     private func setupButtons() {
+     let titles = ["Toplama", "Çıkarma", "Çarpma", "Bölme", "Karışık"]
+     let expressions: [MathExpression.ExpressionType] = [.addition, .subtraction, .multiplication, .division, .mixed]
+     
+     for (index, title) in titles.enumerated() {
+     let button = createHexButton(title: title)
+     button.tag = index
+     button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+     stackView.addArrangedSubview(button)
+     }
+     
+     
+     */
     
     @objc private func buttonTapped(_ sender: UIButton) {
         let expressions: [MathExpression.ExpressionType] = [.addition, .subtraction, .multiplication, .division, .mixed]
@@ -54,72 +81,139 @@ class CategoryVC: UIViewController {
             destinationVC.selectedExpressionType = selectedExpression
         }
     }
+    
 }
 
 extension CategoryVC {
-    private func createHexButton(title: String) -> UIButton {
-        let button = UIButton(type: .system)
-        button.setTitle(title, for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        let hexSize = CGSize(width: 140, height: 120)
-        button.frame = CGRect(origin: .zero, size: hexSize)
-        button.clipsToBounds = true
-        
-        let hexPath = createHexagonPath(in: CGRect(origin: .zero, size: hexSize))
-        applyHexagonMask(to: button, path: hexPath)
-        
-        let gradientColors = [UIColor.systemBlue.cgColor, UIColor.systemPurple.cgColor]
-        applyGradient(to: button, colors: gradientColors, path: hexPath)
-        
-        applyShadow(to: button)
-        return button
-    }
     
-    private func applyHexagonMask(to view: UIView, path: UIBezierPath) {
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = path.cgPath
-        view.layer.mask = shapeLayer
-    }
-    
-    private func applyGradient(to view: UIView, colors: [CGColor], path: UIBezierPath) {
+    func configureButton(_ button : UIButton,title: String,imageName: String ){
+        
+        button.layer.cornerRadius = 12
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor(red: 1.0, green: 0.8627, blue: 0.0, alpha: 1.0).cgColor
+        button.backgroundColor = .clear
+        
         let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = colors
+        //        gradientLayer.colors = [
+        //            UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1).cgColor, // Koyu gri
+        //            UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 1).cgColor, // Orta gri
+        //            UIColor(red: 80/255, green: 80/255, blue: 80/255, alpha: 1).cgColor, // Açık gri
+        //            UIColor(red: 200/255, green: 150/255, blue: 50/255, alpha: 1).cgColor  // Altın sarısı parıltı
+        //        ]
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
         gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-        gradientLayer.frame = view.layer.bounds
         
-        let maskLayer = CAShapeLayer()
-        maskLayer.path = path.cgPath
-        gradientLayer.mask = maskLayer
         
-        view.layer.insertSublayer(gradientLayer, at: 0)
+        if let superview = button.superview {
+            let adjustedFrame = superview.convert(button.frame, to: superview)
+            let backgroundView = UIView(frame: adjustedFrame)
+            gradientLayer.frame = CGRect(origin: .zero, size: adjustedFrame.size)
+            gradientLayer.cornerRadius = button.layer.cornerRadius
+            
+            backgroundView.layer.insertSublayer(gradientLayer, at: 0)
+            backgroundView.layer.cornerRadius = button.layer.cornerRadius
+            // backgroundView.layer.shadowColor = UIColor.purple.cgColor
+            backgroundView.layer.shadowOffset = CGSize(width: 0, height: 3)
+            backgroundView.layer.shadowOpacity = 0.3
+            backgroundView.layer.shadowRadius = 6
+            backgroundView.layer.masksToBounds = false
+            
+            superview.addSubview(backgroundView)
+            superview.bringSubviewToFront(button)
+        }
+        
+        
+//        let stackView = UIStackView()
+//               stackView.axis = .vertical
+//               stackView.alignment = .center
+//               stackView.spacing = 8
+//               stackView.frame = button.bounds
+//               
+//               let imageView = UIImageView()
+//               imageView.image = UIImage(named: imageName)
+//               imageView.contentMode = .scaleAspectFit
+//               imageView.frame = CGRect(x: 0, y: 0, width: button.frame.width * 0.6, height: button.frame.height * 0.5)
+//               
+//               let label = UILabel()
+//               label.text = title
+//               label.textColor = .white
+//               label.font = UIFont.boldSystemFont(ofSize: 18)
+//               label.textAlignment = .center
+//               
+//               stackView.addArrangedSubview(imageView)
+//               stackView.addArrangedSubview(label)
+//               button.addSubview(stackView)
+        
     }
     
-    private func applyShadow(to view: UIView) {
-        view.layer.shadowColor = UIColor.black.cgColor
-        view.layer.shadowOffset = CGSize(width: 3, height: 4)
-        view.layer.shadowOpacity = 0.4
-        view.layer.shadowRadius = 5
-        view.layer.masksToBounds = false
-    }
     
-    private func createHexagonPath(in rect: CGRect) -> UIBezierPath {
-        let path = UIBezierPath()
-        let width = rect.width
-        let height = rect.height
-        let sideLength = width / 2
-        let deltaY = sideLength * sqrt(3) / 2
-        
-        path.move(to: CGPoint(x: width * 0.5, y: 0))
-        path.addLine(to: CGPoint(x: width, y: deltaY))
-        path.addLine(to: CGPoint(x: width, y: height - deltaY))
-        path.addLine(to: CGPoint(x: width * 0.5, y: height))
-        path.addLine(to: CGPoint(x: 0, y: height - deltaY))
-        path.addLine(to: CGPoint(x: 0, y: deltaY))
-        path.close()
-        
-        return path
-    }
+    
+    
+    /*
+     private func createHexButton(title: String) -> UIButton {
+     let button = UIButton(type: .system)
+     button.setTitle(title, for: .normal)
+     button.setTitleColor(.white, for: .normal)
+     button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+     
+     let hexSize = CGSize(width: 140, height: 120)
+     button.frame = CGRect(origin: .zero, size: hexSize)
+     button.clipsToBounds = true
+     
+     let hexPath = createHexagonPath(in: CGRect(origin: .zero, size: hexSize))
+     applyHexagonMask(to: button, path: hexPath)
+     
+     let gradientColors = [UIColor.systemBlue.cgColor, UIColor.systemPurple.cgColor]
+     applyGradient(to: button, colors: gradientColors, path: hexPath)
+     
+     applyShadow(to: button)
+     return button
+     }
+     
+     private func applyHexagonMask(to view: UIView, path: UIBezierPath) {
+     let shapeLayer = CAShapeLayer()
+     shapeLayer.path = path.cgPath
+     view.layer.mask = shapeLayer
+     }
+     
+     private func applyGradient(to view: UIView, colors: [CGColor], path: UIBezierPath) {
+     let gradientLayer = CAGradientLayer()
+     gradientLayer.colors = colors
+     gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+     gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+     gradientLayer.frame = view.layer.bounds
+     
+     let maskLayer = CAShapeLayer()
+     maskLayer.path = path.cgPath
+     gradientLayer.mask = maskLayer
+     
+     view.layer.insertSublayer(gradientLayer, at: 0)
+     }
+     
+     private func applyShadow(to view: UIView) {
+     view.layer.shadowColor = UIColor.black.cgColor
+     view.layer.shadowOffset = CGSize(width: 3, height: 4)
+     view.layer.shadowOpacity = 0.4
+     view.layer.shadowRadius = 5
+     view.layer.masksToBounds = false
+     }
+     
+     private func createHexagonPath(in rect: CGRect) -> UIBezierPath {
+     let path = UIBezierPath()
+     let width = rect.width
+     let height = rect.height
+     let sideLength = width / 2
+     let deltaY = sideLength * sqrt(3) / 2
+     
+     path.move(to: CGPoint(x: width * 0.5, y: 0))
+     path.addLine(to: CGPoint(x: width, y: deltaY))
+     path.addLine(to: CGPoint(x: width, y: height - deltaY))
+     path.addLine(to: CGPoint(x: width * 0.5, y: height))
+     path.addLine(to: CGPoint(x: 0, y: height - deltaY))
+     path.addLine(to: CGPoint(x: 0, y: deltaY))
+     path.close()
+     
+     return path
+     }
+     */
 }
--- MathQuizMastery/uı/view/StartVC.swift
+++ MathQuizMastery/uı/view/StartVC.swift
@@ -8,30 +8,127 @@
 import UIKit
 
 class StartVC: UIViewController {
-
+    
+    @IBOutlet weak var userInfoStackView: UIStackView!
     @IBOutlet weak var buttonStartLabel: UIButton!
+    
+    @IBOutlet weak var userIDIcon: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameStackView: UIStackView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var goldStackView: UIStackView!
+    @IBOutlet weak var goldLabel: UILabel!
+    @IBOutlet weak var goldIcon: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationItem.hidesBackButton = true
         configureButton(buttonStartLabel)
+        setupUserInfoView()
     }
-    
-
-   
-
 }
 
-extension StartVC {
-    func configureButton(_ button : UIButton){
+
+//Mark: - UI Setup
+private extension StartVC {
+    func setupUserInfoView(){
+        configureUserInfoStackView()
+        configureAvatarImageView()
+        configureUsernameStackView()
+        configureGoldStackView()
+        layoutUserInfoView()
+    }
+}
+
+// Mark: - UI Configuration
+private extension StartVC{
+    func configureUserInfoStackView (){
+        userInfoStackView.axis = .horizontal
+        userInfoStackView.alignment = .center
+        userInfoStackView.distribution = .equalSpacing
+        userInfoStackView.spacing = 20
+        userInfoStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userInfoStackView)
+    }
+    
+    func configureAvatarImageView(){
+        avatarImageView.image = UIImage(named: "image2vector")
+        avatarImageView.contentMode = .scaleAspectFit
+        avatarImageView.layer.cornerRadius = 30
+        avatarImageView.clipsToBounds = true
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func configureUsernameStackView(){
+        usernameStackView.axis = .horizontal
+        usernameStackView.alignment = .center
+        usernameStackView.spacing = 5
+        usernameStackView.translatesAutoresizingMaskIntoConstraints = false
         
+        usernameLabel.text = "Username"
+        usernameLabel.font = .systemFont(ofSize: 20,weight: .semibold)
+        usernameLabel.textColor = .black
+        
+        userIDIcon.image = UIImage(named: "user_id_icon")
+        userIDIcon.contentMode = .scaleAspectFit
+        
+        usernameStackView.addArrangedSubview(userIDIcon)
+        usernameStackView.addArrangedSubview(usernameLabel)
+    }
+    
+    func configureGoldStackView(){
+        goldStackView.axis = .horizontal
+        goldStackView.alignment = .center
+        goldStackView.spacing = 5
+        goldStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        goldLabel.text = "1000"
+        goldLabel.font = .systemFont(ofSize: 18, weight: .semibold)
+        goldLabel.textColor = .yellow
+        
+        goldIcon.image = UIImage(named: "circle_circle_symbol")
+        goldIcon.contentMode = .scaleAspectFit
+        
+        goldStackView.addArrangedSubview(goldLabel)
+        goldStackView.addArrangedSubview(goldIcon)
+    }
+}
+
+
+// MARK: - Layout
+private extension StartVC{
+    func layoutUserInfoView() {
+        userInfoStackView.addArrangedSubview(usernameStackView)
+        userInfoStackView.addArrangedSubview(avatarImageView)
+        userInfoStackView.addArrangedSubview(goldStackView)
+        
+        NSLayoutConstraint.activate([
+            userInfoStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            userInfoStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            userInfoStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            userInfoStackView.heightAnchor.constraint(equalToConstant: 60),
+            
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            
+            userIDIcon.widthAnchor.constraint(equalToConstant: 18),
+            userIDIcon.heightAnchor.constraint(equalToConstant: 18),
+            
+            goldIcon.widthAnchor.constraint(equalToConstant: 18),
+            goldIcon.heightAnchor.constraint(equalToConstant: 18)
+        ])
+    }
+}
+
+
+private extension StartVC {
+    func configureButton(_ button : UIButton){
         button.layer.cornerRadius = 12
         button.layer.borderWidth = 2
         button.layer.borderColor = UIColor(red: 1.0, green: 0.8627, blue: 0.0, alpha: 1.0).cgColor
         button.backgroundColor = .clear
-        
-        
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [
@@ -40,8 +137,6 @@ extension StartVC {
         ]
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
         gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-        
-        
         
         if let superview = button.superview {
             let adjustedFrame = superview.convert(button.frame, to: superview)
