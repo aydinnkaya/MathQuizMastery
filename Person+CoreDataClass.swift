2d396ddb2dcef678182f7c4adee27ccf07e82079
aydinnkaya <Aydinnkaya1@gmail.com>
2025-03-29 23:01:20 +0300

    updateRegister

--- Person+CoreDataClass.swift
+++ Person+CoreDataClass.swift
@@ -2,13 +2,14 @@
 //  Person+CoreDataClass.swift
 //  MathQuizMastery
 //
-//  Created by AydınKaya on 21.03.2025.
+//  Created by Aydın KAYA on 29.03.2025.
+//
 //
 
 import Foundation
 import CoreData
 
 
-@objc(Person)
 public class Person: NSManagedObject {
+
 }
