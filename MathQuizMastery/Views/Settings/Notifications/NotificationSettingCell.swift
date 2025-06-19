4697005547f7e9b771e3c2337a50aeb7e3204819
Aydın Kaya <Aydinnkaya1@gmail.com>
2025-06-19 09:43:17 +0300

    NotificationSettingCell

---
+++ MathQuizMastery/Views/Settings/Notifications/NotificationSettingCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationSettingCell.swift
+//  MathQuizMastery
+//
+//  Created by AydınKaya on 19.06.2025.
+//
+
+import UIKit
+
+class NotificationSettingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MathQuizMastery/Views/Settings/Notifications/NotificationSettingsVC.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationSettingsVC.swift
+//  MathQuizMastery
+//
+//  Created by AydınKaya on 19.06.2025.
+//
+
+import UIKit
+
+class NotificationSettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    
+
+}
